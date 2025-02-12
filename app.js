c10dea0e564d845938b681a406f13dbec024c405
nandinigummalla <nandinigummalla27@gmail.com>
2025-02-12 21:22:09 +0530

    "first commit"

---
+++ app.js
@@ -0,0 +1,13 @@
+const head = React.createElement("h1", { id: "heading" }, "Hie from React");
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "Hie I am sib"),
+    React.createElement("h1", {}, "Hie I am sib"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "Hie I am child1"),
+    React.createElement("h1", {}, "Hie I am child2"),
+  ]),
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
