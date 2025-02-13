a7b918fc803598f5ae0307a2fead30d4cbc0d091
nandinigummalla <nandinigummalla27@gmail.com>
2025-02-13 22:25:01 +0530

    second commit

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const head = React.createElement("h1", { id: "heading" }, "Hie from React");
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
